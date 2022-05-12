e14d60a2a656f18062bd9dbb57a5423364356e70
Web Developer <useraccount@WebDeveloperMBP.local>
2022-05-12 15:21:48 -0500

    Refactor Functions - LM

--- react/CompareTable.tsx
+++ react/CompareTable.tsx
@@ -14,6 +14,7 @@ interface TableDataObject {
 
 interface ProductObject {
   title: string
+  image: string
   shopLink: string
   features: Array<FeatureObject>
 }
@@ -33,11 +34,15 @@ const CompareTable: StorefrontFunctionComponent<CompareTableProps> = ({ tableDat
   const [featureTitles, setFeatureTitles] = useState<Array<string>>([]);
   const [allProducts, setAllProducts] = useState<Array<ProductObject>>([]);
   const [featureData, setFeatureData] = useState<Array<any>>([]);
+  const [productImages, setProductImages] = useState<Array<string>>([]);
   const [shopLinks, setShopLinks] = useState<Array<string>>([]);
   const [thWidth, setThWidth] = useState<number>(0);
+  const [tableContainerHeight, setTableContainerHeight] = useState<Number>(0);
 
   const classPrefix: string = "eriksbikeshop-comparetable-1-x-";
   const hightlightClassName: string = "redBackground"
+  const rowHeight: number = 2.5;
+  const imageHeight: number = 5;
 
   useEffect(() => {
     if (!openGate) return;
@@ -52,79 +57,71 @@ const CompareTable: StorefrontFunctionComponent<CompareTableProps> = ({ tableDat
     const shopLinkTemp: Array<string> = [];
     tableData.products.forEach(shopLink => shopLinkTemp.push(shopLink.shopLink));
 
+    const productImagesTemp: Array<string> = [];
+    tableData.products.forEach(image => productImagesTemp.push(image.image));
+
     setFeatureData(featureValuesTemp);
     setFeatureTitles(featureTitleTemp);
     setAllProducts(tableData.products);
+    setProductImages(productImagesTemp);
     setShopLinks(shopLinkTemp);
     setThWidth(100 / (tableData.products.length + 1));
+    setTableContainerHeight((featureTitleTemp.length + 3) * rowHeight + imageHeight);
     setOpenGate(false);
   })
 
-  const handleHighlight = (e: any) => {
-    toggleHighlight(true, grabActiveCell(e.target.id));
+  const handleToggle = (e: any) => {
+    const light = e.type === "mouseover";
+    const cellInfo = grabActive(e.target);
+    const { activeRow, activeCol } = cellInfo;
+
+    // @ts-expect-error 
+    const redCol: any = document.getElementById(`col-${activeCol}`);
+    light ? redCol.classList.add(classPrefix + hightlightClassName) : redCol.classList.remove(classPrefix + hightlightClassName);
+
+    if (activeRow < featureTitles.length) {
+      // @ts-expect-error
+      const redRow: any = document.getElementById(`row-${activeRow}`);
+      light ? redRow.classList.add(classPrefix + hightlightClassName) : redRow.classList.remove(classPrefix + hightlightClassName);
+    }
   }
 
-  const handleDim = (e: any) => {
-    toggleHighlight(false, grabActiveCell(e.target.id));
-  }
-
-  const handleButtonHighlight = (e: any) => {
-    toggleHighlightButton(true, grabActiveButton(e.target.id));
-  }
-
-  const handleButtonDim = (e: any) => {
-    toggleHighlightButton(false, grabActiveButton(e.target.id));
-  }
-
-  const grabActiveButton = (activeButton: string) => Number(activeButton.split("cell-")[1]);
-
-  const grabActiveCell = (activeCell: string) => {
-    const activeRow: Number = Number(activeCell.split("row-")[1].split("-cell")[0]);
-    const activeCol: Number = Number(activeCell.split("-cell-")[1]);
+  const grabActive = (activeElement: any) => {
+    const activeRow: number = Number(activeElement.id.split("row-")[1].split("-cell")[0]);
+    const activeCol: number = Number(activeElement.id.split("-cell-")[1]);
     return { activeRow, activeCol };
   }
 
-  const toggleHighlightButton = (light: Boolean, col: Number) => {
-    // @ts-expect-error
-    const redCol: any = document.getElementById(`col-${col}`);
-    light ? redCol.classList.add(classPrefix + hightlightClassName) : redCol.classList.remove(classPrefix + hightlightClassName);
-  }
-
-  const toggleHighlight = (light: Boolean, info: HighlightObject) => {
-    const { activeRow, activeCol } = info;
-
-    // @ts-expect-error
-    const redRow: any = document.getElementById(`row-${activeRow}`);
-
-    // @ts-expect-error
-    const redCol: any = document.getElementById(`col-${activeCol}`);
-
-    if (redRow) light ? redRow.classList.add(classPrefix + hightlightClassName) : redRow.classList.remove(classPrefix + hightlightClassName);
-    light ? redCol.classList.add(classPrefix + hightlightClassName) : redCol.classList.remove(classPrefix + hightlightClassName);
-  }
-
   return (
-    <div className={styles.tableContainer}>
+    <div style={{ height: `${tableContainerHeight}rem` }} className={styles.tableContainer}>
       <table>
         <tr>
-          <th style={{ width: `${thWidth}%` }} className={styles.noBorder}></th>
+          <td className={styles.noBorderImageCell}></td>
+          {productImages.map(image => (
+            <td key={`td-${image}`} className={styles.noBorderImageCell}>
+              <img key={`image-${image}`} src={image} style={{ height: `${imageHeight}rem` }} />
+            </td>
+          ))}
+        </tr>
+        <tr>
+          <th style={{ width: `${thWidth}%` }} className={styles.noBorderTh}></th>
           {allProducts.map((product, index) => (
-            <th key={`col-${index}`} id={`col-${index}`} style={{ width: `${thWidth}%` }}>{product.title}</th>
+            <th key={`col-${index}`} id={`col-${index}`} style={{ width: `${thWidth}%`, height: `${rowHeight}rem` }}>{product.title}</th>
           ))}
         </tr>
         {featureTitles.map((feature, index) => (
-          <tr>
-            <td id={`row-${index}`} className={styles.featureTitle}>{feature}</td>
+          <tr key={`tr-${index}`}>
+            <td key={`row-${index}`} id={`row-${index}`} style={{ height: `${rowHeight}rem` }} className={styles.featureTitle}><span>{feature}</span></td>
             {featureData.map((data, featureIndex) => (
-              <td key={`row-${index}-cell-${featureIndex}`} id={`row-${index}-cell-${featureIndex}`} onMouseOver={handleHighlight} onMouseOut={handleDim}>{data[index].value}</td>
+              <td key={`row-${index}-cell-${featureIndex}`} id={`row-${index}-cell-${featureIndex}`} onMouseOver={handleToggle} onMouseOut={handleToggle}>{data[index].value}</td>
             ))}
           </tr>
         ))}
         <tr>
           <td style={{ width: `${thWidth}%` }} className={styles.noBorderShop}></td>
           {shopLinks.map((shopLink, index) => (
-            <td key={`row-${featureTitles.length}-cell-${index}`} id={`row-${featureTitles.length}-cell-${index}`} onMouseOver={handleHighlight} onMouseOut={handleDim} className={styles.shopButtonCell}>
-              <a id={`row-${featureTitles.length}-cell-${index}`} href={shopLink} target="_blank" rel="noreferrer" onMouseOver={handleButtonHighlight} onMouseOut={handleButtonDim} className={styles.shopNowTableButton}>Shop Now</a>
+            <td key={`row-${featureTitles.length}-cell-${index}`} className={styles.shopButtonCell}>
+              <a id={`row-${featureTitles.length}-cell-${index}`} href={shopLink} target="_blank" rel="noreferrer" onMouseOver={handleToggle} onMouseOut={handleToggle} className={styles.shopNowTableButton}>Shop Now</a>
             </td>
           ))}
         </tr>
